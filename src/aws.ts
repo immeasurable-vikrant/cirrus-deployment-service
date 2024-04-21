2fbfd47c5084853d493ac745dfdf04b26ba78b3a
Vikrant <vikrant.immeasurable@gmail.com>
2024-04-21 22:00:29 +0530

    added copy to final dist with only html css and js files

--- src/aws.ts
+++ src/aws.ts
@@ -3,10 +3,10 @@ import fs from "fs";
 import path from "path";
 
 const s3 = new S3({
-    accessKeyId: "7ea9c3f8c7f0f26f0d21c5ce99d1ad6a",
-    secretAccessKey: "b4df203781dd711223ce931a2d7ca269cdbf81bb530de4548474584951b798be",
-    endpoint: "https://e21220f4758c0870ba9c388712d42ef2.r2.cloudflarestorage.com"
-})
+	accessKeyId: process.env.ACCESS_KEY_ID,
+	secretAccessKey: process.env.SECRET_ACCESS_KEY,
+	endpoint: process.env.END_POINT,
+});
 
 // output/asdasd
 export async function downloadS3Folder(prefix: string) {
@@ -39,4 +39,36 @@ export async function downloadS3Folder(prefix: string) {
     console.log("awaiting");
 
     await Promise.all(allPromises?.filter(x => x !== undefined));
+}
+
+export function copyFinalDist(id: string) {
+    const folderPath = path.join(__dirname, `output/${id}/dist`);
+    const allFiles = getAllFiles(folderPath);
+    allFiles.forEach(file => {
+        uploadFile(`dist/${id}/` + file.slice(folderPath.length + 1), file);
+    })
+}
+
+const getAllFiles = (folderPath: string) => {
+    let response: string[] = [];
+
+    const allFilesAndFolders = fs.readdirSync(folderPath);allFilesAndFolders.forEach(file => {
+        const fullFilePath = path.join(folderPath, file);
+        if (fs.statSync(fullFilePath).isDirectory()) {
+            response = response.concat(getAllFiles(fullFilePath))
+        } else {
+            response.push(fullFilePath);
+        }
+    });
+    return response;
+}
+
+const uploadFile = async (fileName: string, localFilePath: string) => {
+    const fileContent = fs.readFileSync(localFilePath);
+    const response = await s3.upload({
+        Body: fileContent,
+        Bucket: "vercel",
+        Key: fileName,
+    }).promise();
+    console.log(response);
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-import { createClient, commandOptions } from 'redis';
-import { downloadS3Folder } from './aws';
-import { buildProject } from './utils';
+
+import { createClient, commandOptions } from "redis";
+import { copyFinalDist, downloadS3Folder } from "./aws";
+import { buildProject } from "./utils";
 const subscriber = createClient();
 subscriber.connect();
 
@@ -8,17 +9,19 @@ const publisher = createClient();
 publisher.connect();
 
 async function main() {
-	while (1) {
-		const res = await subscriber.brPop(
-			commandOptions({ isolated: true }),
-			'build-queue',
-			0
-		);
-		// @ts-ignore;
-		const id = res.element;
-
-		await downloadS3Folder(`output/${id}`);
-		await buildProject(id);
-	}
+    while(1) {
+        const res = await subscriber.brPop(
+            commandOptions({ isolated: true }),
+            'build-queue',
+            0
+          );
+        // @ts-ignore;
+        const id = res.element
+        
+        await downloadS3Folder(`output/${id}`)
+        await buildProject(id);
+        copyFinalDist(id);
+        publisher.hSet("status", id, "deployed")
+    }
 }
 main();
