31735bf996bfa3196374cdf81302700b3f11ed95
Vikrant <vikrant.immeasurable@gmail.com>
2024-04-21 22:09:44 +0530

    refactored files

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
-
-import { createClient, commandOptions } from "redis";
-import { copyFinalDist, downloadS3Folder } from "./aws";
-import { buildProject } from "./utils";
+import { createClient, commandOptions } from 'redis';
+import { copyFinalDist, downloadS3Folder } from './aws';
+import { buildProject } from './utils';
 const subscriber = createClient();
 subscriber.connect();
 
@@ -9,19 +8,19 @@ const publisher = createClient();
 publisher.connect();
 
 async function main() {
-    while(1) {
-        const res = await subscriber.brPop(
-            commandOptions({ isolated: true }),
-            'build-queue',
-            0
-          );
-        // @ts-ignore;
-        const id = res.element
-        
-        await downloadS3Folder(`output/${id}`)
-        await buildProject(id);
-        copyFinalDist(id);
-        publisher.hSet("status", id, "deployed")
-    }
+	while (1) {
+		const res = await subscriber.brPop(
+			commandOptions({ isolated: true }),
+			'build-queue',
+			0
+		);
+		// @ts-ignore;
+		const id = res.element;
+
+		await downloadS3Folder(`output/${id}`);
+		await buildProject(id);
+		copyFinalDist(id);
+		publisher.hSet('status', id, 'deployed');
+	}
 }
 main();
