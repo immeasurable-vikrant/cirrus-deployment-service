7429f2e3ac8fdb27e39e063faab033c2fb272ee9
Vikrant <vikrant.immeasurable@gmail.com>
2024-04-21 21:31:41 +0530

    redis queueing the ids (project) for deployment service

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+
+import { createClient, commandOptions } from "redis";
+const subscriber = createClient();
+subscriber.connect();
+
+async function main() {
+    while(1) {
+        const res = await subscriber.brPop(
+            commandOptions({ isolated: true }),
+            'build-queue',
+            0
+          );
+        // @ts-ignore;
+        const id = res.element
+    }
+}
+main();
